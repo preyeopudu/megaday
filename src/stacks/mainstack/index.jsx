2314d90746e6d334875d8b0086f18e9b8941ea45
unknown <preyeopudu@gmail.com>
2022-09-07 18:10:02 +0100

    installed bottom tabs

--- src/stacks/mainstack/index.jsx
+++ src/stacks/mainstack/index.jsx
@@ -5,7 +5,7 @@ const Stack = createNativeStackNavigator();
 
 const MainStack = () => {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="Dashboard" component={DashboardScreen} />
     </Stack.Navigator>
   );
