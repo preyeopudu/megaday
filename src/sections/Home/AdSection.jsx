3b372bdf83e4564cb76c825cef40e96107ef38cf
unknown <preyeopudu@gmail.com>
2022-09-08 23:21:50 +0100

    Completed ad section

--- src/sections/Home/AdSection.jsx
+++ src/sections/Home/AdSection.jsx
@@ -5,6 +5,8 @@ import {
   Text,
 } from "react-native";
 import AutoHeightImage from "react-native-auto-height-image";
+import numbro from "numbro";
+import { primary, secondary } from "../../constants/color";
 
 const AdSection = () => {
   const { width } = useWindowDimensions();
@@ -13,18 +15,26 @@ const AdSection = () => {
       activeOpacity={0.8}
       style={{
         marginTop: 20,
-        alignItems: "center",
       }}
     >
       <AutoHeightImage
-        width={width * 0.8}
+        style={{ alignSelf: "center", marginBottom: 10 }}
+        width={width * 0.85}
         source={require("../../../assets/images/car.png")}
       />
-      <View>
+      <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
         <View>
-          <Text>Nike Running Shoe</Text>
-          <Text>shoes</Text>
+          <Text style={{ fontFamily: "bold", fontSize: 14 }}>
+            Nike Running Shoe
+          </Text>
+          <Text style={{ fontSize: 10, fontFamily: "regular", color: primary }}>
+            shoes
+          </Text>
         </View>
+        <Text style={{ fontFamily: "bold", color: secondary, fontSize: 16 }}>
+          {"\u20A6"}
+          {numbro(17000).format({ thousandSeparated: true })}
+        </Text>
       </View>
     </TouchableOpacity>
   );
