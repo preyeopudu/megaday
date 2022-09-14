e69cab1f15a9ea411f4265141769940c812036eb
unknown <preyeopudu@gmail.com>
2022-09-14 08:08:17 +0100

    installed rn-emoji

--- src/sections/Home/ChatInput.jsx
+++ src/sections/Home/ChatInput.jsx
@@ -1,8 +1,28 @@
 import { Entypo, Feather, FontAwesome5 } from "@expo/vector-icons";
 import { View, TextInput, TouchableOpacity } from "react-native";
 import { primary } from "../../constants/color";
+import { useState } from "react";
+import * as ImagePicker from "expo-image-picker";
 
 const ChatInput = () => {
+  const [image, setImage] = useState(null);
+
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.cancelled) {
+      setImage(result.uri);
+    }
+  };
+
   return (
     <View
       style={{
@@ -36,7 +56,7 @@ const ChatInput = () => {
       </View>
 
       <View style={{ flexDirection: "row", alignItems: "center" }}>
-        <TouchableOpacity>
+        <TouchableOpacity onPress={pickImage}>
           <Feather
             name="paperclip"
             size={20}
